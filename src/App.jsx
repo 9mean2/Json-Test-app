94904ff2d6743ab86e07e4f73891991098f63e5d
9mean2 <o3omoomin@gmail.com>
2023-02-17 15:54:11 +0900

    feat: 주석 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
+  // JSON 으로 부터  가져온 데이털르 저장할 변수 state를 선언 [데이터가 배열 형태이기 때문에 기본값에 배열을 넣어줌]
   const [data, setData] = useState([]);
 
+  //컴포넌트가 처음에 마운트 될 때 사용
   useEffect(() => {
+    //fetch 라는 내장 함수로 읽어옴 , fetch로 가져오면 자바스크립트 형태로 남음
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((json) => {
+        //state에 결과값 저장 ...으로 풀어주고 []을 사용해 다시 배열에 넣음 , 원래 배열이었으니까
         setData([...json]);
         return console.log(json);
       });
